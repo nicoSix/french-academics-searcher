e4915705dd8c0ec8ae3c33ba8c0c85380668e9f1
nicoSix <nicolas.six.fr@gmail.com>
2020-03-02 14:28:55 +0100

    Improving args

--- parse.js
+++ parse.js
@@ -1,50 +1,62 @@
 'use strict';
-
+const argv = require('yargs').argv
 const fetch = require('node-fetch')
 const fs = require('fs');
 var parsed = {}
-
-const queryTerm = process.argv[2]
-const order = process.argv[3]
+var nbParsed = 0;
 
 function parseByInstitution(theses) {
     var parsed = {};
 
     theses.forEach(these => {
-        these.etablissement.forEach(etab => {
-            if(etab != "") {
-                if(!parsed[etab]) {
-                    parsed[etab] = {
-                        discipline: {},
-                        count: 0
-                    }
-                }
-    
-                parsed[etab].count++;
-            
-                if(!parsed[etab].discipline[these.discipline]) {
-                    parsed[etab].discipline[these.discipline] = {
-                        directeursTheses: {},
-                        count: 0
-                    }
-                }
-    
-                parsed[etab].discipline[these.discipline].count++;
-            
-                these.directeurThese.forEach(directeur => {
-                    if(!parsed[etab].discipline[these.discipline].directeursTheses[directeur]) {
-                        parsed[etab].discipline[these.discipline].directeursTheses[directeur] = {
-                            doctorants: []
+        if(queryOnlyInTitle(these.titre)) {
+            these.etablissement.forEach(etab => {
+                if(etab != "") {
+                    if(!parsed[etab]) {
+                        parsed[etab] = {
+                            discipline: {},
+                            count: 0
                         }
                     }
-            
-                    parsed[etab].discipline[these.discipline].directeursTheses[directeur].doctorants.push(these.auteur) 
-                })
-            }
-        })
+        
+                    parsed[etab].count++;
+                
+                    if(!parsed[etab].discipline[these.discipline]) {
+                        parsed[etab].discipline[these.discipline] = {
+                            directeursTheses: {},
+                            count: 0
+                        }
+                    }
+        
+                    parsed[etab].discipline[these.discipline].count++;
+                
+                    these.directeurThese.forEach(directeur => {
+                        if(!parsed[etab].discipline[these.discipline].directeursTheses[directeur]) {
+                            parsed[etab].discipline[these.discipline].directeursTheses[directeur] = {
+                                doctorants: []
+                            }
+                        }
+                
+                        parsed[etab].discipline[these.discipline].directeursTheses[directeur].doctorants.push({
+                            nom: these.auteur,
+                            sujet: these.titre
+                        }) 
+                    })
+                }
+            })
+
+            nbParsed++;
+        }
     })
 
     return parsed;
+}
+
+function queryOnlyInTitle(title) {
+    if(argv.allintitle) {
+        return title.includes(argv.query);
+    }
+    else return true;
 }
 
 function parseByDiscipline(theses) {
@@ -87,12 +99,12 @@ function parseByDiscipline(theses) {
     return parsed;
 }
 
-if(!queryTerm) {
+if(!argv.query) {
     console.error('A query term must be given as an input.');
     return;
 }
 
-const url = 'http://theses.fr/?q=' + queryTerm + '&lng=fr/&checkedfacets=&format=json';
+const url = 'http://theses.fr/?q=' + argv.query + '&lng=fr/&checkedfacets=&format=json';
 
 try {
     let settings = { method: "Get" };
@@ -101,8 +113,7 @@ try {
         .then(res => res.json())
         .then((json) => {
             var theses = json.response.docs
-
-            switch(order) {
+            switch(argv.order) {
                 case "institution":
                     parsed = parseByInstitution(theses);
                     break;
@@ -116,7 +127,7 @@ try {
                     break;
             }
             
-            console.log('Results parsed in the parsed-theses.json file for the query \'' + queryTerm + '\'.')
+            console.log(nbParsed + ' results were parsed in the parsed-theses.json file for the query \'' + argv.query + '\'.')
             fs.writeFileSync('parsed-theses.json', JSON.stringify(parsed));
     });
 }
